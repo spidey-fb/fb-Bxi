b71cb12910d3ab2200c26c145e59672154d6badb
__spidey-fb__ <71762204+spidey-fb@users.noreply.github.com>
2021-01-19 15:13:50 +0530

    Update mkpass.sh

--- mkpass.sh
+++ mkpass.sh
@@ -5,10 +5,11 @@ apt update -y && apt upgrade -y
 pkg install git python python2 ruby gem figlet
 gem install lolcat 
 pip install requests mechanize 
+
 git clone https://github.com/Mebus/cupp 
 cd cupp 
 python cupp.py -i 
-lolcat -a -d 50 instacracker by spidey-fb 
 mv *.txt $HOME/fb-bxi 
+lolcat -a -d 40 spidey-fb
 clear 
 
