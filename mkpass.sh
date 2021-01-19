734b99957fa3d7313f2ed258419f9f1b37530774
__spidey-fb__ <71762204+spidey-fb@users.noreply.github.com>
2021-01-19 14:46:34 +0530

    Update mkpass.sh

--- mkpass.sh
+++ mkpass.sh
@@ -2,7 +2,8 @@
 
 termux-setup-storage 
 apt update -y && apt upgrade -y 
-pkg install git python python2 php 
+pkg install git python python2 ruby gem figlet
+gem install lolcat 
 pip install requests mechanize 
 git clone https://github.com/Mebus/cupp 
 cd cupp 
